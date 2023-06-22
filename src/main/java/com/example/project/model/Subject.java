5068848e23a09dbb68821095186b471f7d937648
Alec Hannaford <alec322@gmail.com>
2023-06-21 18:39:44 -0700

    subject model but idk if it has foreign key properly set up yet

---
+++ src/main/java/com/example/project/model/Subject.java
@@ -0,0 +1,54 @@
+package com.example.project.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@Table(name="subjects")
+public class Subject {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "subjects_generator")
+    private long id;
+
+
+    @Column(name="name")
+    private String name;
+
+    @Lob
+    private String description;
+
+    //Fetchtype lazy tells Hibernate(ORM) to only fetch the related entites when needed
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "king_id", nullable = false)
+    //Delete children upon deletion
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private King king;
+
+    //Getters n setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setKing(King king) {
+        this.king = king;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public King getKing() {
+        return king;
+    }
+}
