983a28b798e13b9e95e0acdbabecddc4f419ed51
Alec Hannaford <alec322@gmail.com>
2023-06-21 18:20:16 -0700

    king model

---
+++ src/main/java/com/example/project/model/King.java
@@ -0,0 +1,47 @@
+package com.example.project.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="kings")
+public class King {
+   @Id
+   @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "king_generator")
+   private long id;
+
+
+   @Column(name = "title")
+   private String title;
+
+   @Column(name = "name")
+   private String name;
+
+   @Column(name = "isHeathen")
+   private boolean isHeathen;
+
+   //Constructor
+   public King(){}
+
+   public King(String title, String name, boolean isHeathen){
+      this.title = title;
+      this.name = name;
+      this.isHeathen = isHeathen;
+   }
+
+   //Getters and setters
+   public String getTitle(){return title;}
+   public void setTitle(String title){
+      this.title = title;
+   }
+
+   public String getName(){return name;}
+   public void setName(String name){
+      this.name = name;
+   }
+
+   public boolean getIsHeathen(){return isHeathen;}
+   public void setIsHeathen(boolean isHeathen){
+      this.isHeathen = isHeathen;
+   }
+
+}
